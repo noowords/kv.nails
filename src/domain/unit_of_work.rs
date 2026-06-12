02441c1e156b7fa2d699db7d4ba5547ff879ac77
no words <noowords@mail.ru>
2026-06-12 09:28:22 +0900

    Removing irrelevant files

--- src/domain/unit_of_work.rs
+++
@@ -1,7 +0,0 @@
-use async_trait::{ async_trait };
-
-#[async_trait]
-pub trait UnitOfWork: Send + Sync {
-    async fn commit(self: Box<Self>) -> Result<(), String>;
-    async fn rollback(self: Box<Self>) -> Result<(), String>;
-}
